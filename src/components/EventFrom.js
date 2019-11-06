5386ef35232d4394c76fdfb570ca149ae7cef810
dorodero <doro_dero_bah@yahoo.co.jp>
2019-11-06 22:03:26 +0900

    delete all operation logs

--- src/components/EventFrom.js
+++ src/components/EventFrom.js
@@ -46,7 +46,16 @@ const EventFrom = () => {
     }
   
     const unCreateble = title ==='' || body ===''
-  
+    
+    const deleteAllOperationLogs = e => {
+      e.preventDefault()
+      const result = window.confirm('全ての操作ログを本当に削除しても良いですか？')
+      if (result) {
+        dispatch({
+          type: DELETE_ALL_OPARATION_LOGS
+        })
+      }
+    }
     return (
         <>
             <h4>イベントフォーム作成</h4>
@@ -61,6 +70,7 @@ const EventFrom = () => {
                 </div>
                 <button className="btn btn-primary" onClick={addEvent} disabled={unCreateble}>イベントを作成する</button>
                 <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
+                <button className="btn btn-danger" onClick={deleteAllOperationLogs} disabled={state.operationLogs.length === 0}>全ての操作ログを削除する</button>
             </form>
         </>
     )
